a0e13807df2354d878749b0f31a12b8079cb5dc6
mem_dixy <mem_dixy@pm.me>
2022-12-01 05:32:05 +0000

    Raw code for both puzzles.

--- demo.py
+++ demo.py
@@ -1,12 +1,28 @@
+elfs = []
+number = 0
 line = True
+
 with open("elf.txt") as file:
     while line:
         line = file.readline()
         match line:
             case "":
-                print("EOL")
+                line = False
             case "\n":
-                print("break")
+                elfs.append(number)
+                number = 0
             case _:
-                number = int(line.strip())
-                print(number)
+                number += int(line.strip())
+
+print(elfs)
+print(max(elfs))
+
+one = max(elfs)
+elfs.remove(one)
+two = max(elfs)
+elfs.remove(two)
+three = max(elfs)
+elfs.remove(three)
+
+total = one + two + three
+print(total)
