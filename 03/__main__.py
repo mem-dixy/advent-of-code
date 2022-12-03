888123e6c8409f04867776e602631c266d8d1079
mem_dixy <mem_dixy@pm.me>
2022-12-03 06:45:07 +0000

    Part 1.

--- 03/__main__.py
+++ 03/__main__.py
@@ -1,71 +1,42 @@
 import enum
 
-class Shape(enum.Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
-
-class Outcome(enum.Enum):
-    LOST = 0
-    DRAW = 3
-    WON = 6
-
-
 line = True
 score = 0
 total_score = 0
 
-with open("02/input.txt") as file:
+
+def priority(letter):
+    cord = ord(letter)
+    if cord < 97:
+        return cord - 64 + 26
+
+    return cord - 96
+
+
+with open("03/input.txt") as file:
     while line:
-        line = file.readline()
+        line = file.readline().strip()
         if not line:
             break
-        (opponent, you) = tuple(line.split())
-        score = 0
-        match opponent:
-            case "A":
-                opponent = Shape.ROCK
-            case "B":
-                opponent = Shape.PAPER
-            case "C":
-                opponent = Shape.SCISSORS
+        full = len(line)
+        half = full // 2
+        one = line[0:half]
+        two = line[half:full]
+        left = set({})
+        right = set({})
+        for item in one:
+            left.add(item)
+        for item in two:
+            right.add(item)
 
-        match you:
-            case "X":
-                you = Outcome.LOST
-            case "Y":
-                you = Outcome.DRAW
-            case "Z":
-                you = Outcome.WON
+        both = left & right
+        for item in both:
+            letter = item
 
-        match opponent:
-            case Shape.ROCK:
-                match you:
-                    case Outcome.LOST:
-                        outcome = Shape.SCISSORS
-                    case Outcome.DRAW:
-                        outcome = Shape.ROCK
-                    case Outcome.WON:
-                        outcome = Shape.PAPER
-            case Shape.PAPER:
-                match you:
-                    case Outcome.LOST:
-                        outcome = Shape.ROCK
-                    case Outcome.DRAW:
-                        outcome = Shape.PAPER
-                    case Outcome.WON:
-                        outcome = Shape.SCISSORS
-            case Shape.SCISSORS:
-                match you:
-                    case Outcome.LOST:
-                        outcome = Shape.PAPER
-                    case Outcome.DRAW:
-                        outcome = Shape.SCISSORS
-                    case Outcome.WON:
-                        outcome = Shape.ROCK
-
-
-        score = you.value + outcome.value
+        score = priority(letter)
         total_score += score
+
+
+
 
 print(total_score)
