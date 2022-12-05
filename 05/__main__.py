26df5d434fdba567762e225ceb0557efa3d60d8b
mem_dixy <mem_dixy@pm.me>
2022-12-05 05:33:48 +0000

    Stacks formated to account for gaps.

--- 05/__main__.py
+++ 05/__main__.py
@@ -1,24 +1,32 @@
 line = True
 count = 0
 
-def myrange(input):
-    (left, right) = input.split("-")
-    left = int(left)
-    right = int(right)
-    span = range(left, right + 1, 1)
-    span = set(span)
-    return span
+with open("05/input.txt") as file:
+    line = file.readlines()
+    string = "".join(line)
 
-with open("04/input.txt") as file:
-    while line:
-        line = file.readline().strip()
-        if not line:
-            break
-        (left, right) = tuple(line.split(","))
-        left = myrange(left)
-        right = myrange(right)
-        # if left <= right or right <= left:
-        if left & right:
-            count += 1
+    (top, bottom) = string.split("\n\n")
+    top_lines = top.split("\n")
+    length_top_lines = len(top_lines) - 1
+    index = top_lines[length_top_lines]
 
-print(count)
+    STACKS = ((len(index)) // 4) + 1
+    LINE_LENGTH = ((STACKS - 1) * 4) + 3
+
+    top_lines = top_lines[0:length_top_lines]
+    new_top_lines = []
+    for lines in top_lines:
+        pad = lines.ljust(LINE_LENGTH)
+        if pad.startswith(" "):
+            pad = pad.replace("    ", "[_] ", 1)
+        pad = pad.replace("    ", " [_]")
+        new_top_lines.append(pad)
+
+    print(new_top_lines)
+
+
+    cat = "\n".join(new_top_lines)
+
+    print(cat)
+    print("------------")
+    print(bottom)
