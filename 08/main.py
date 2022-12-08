0b65bc12450877979b93986312e6bd19967774df
mem_dixy <mem_dixy@pm.me>
2022-12-08 05:35:27 +0000

    Setup part 1.

--- 08/main.py
+++ 08/main.py
@@ -1,21 +1,50 @@
+import io
 
+grid = []
+
+# 0 min 9 max
+# 1 min 10 max
 with open("input.txt") as file:
-    line = True
-    while line:
-        line = file.readline().strip()
-        match line.split():
-            case ["$", "cd", ".."]:
-                system.move_out()
-            case ["$", "cd", "/"]:
-                system.move_root()
-            case ["$", "cd", directory]:
-                system.move_in(directory)
-            case ["$", "ls"]:
-                pass
-            case ["dir", directory]:
-                system.push_directory(Directory(directory))
-            case [size, name]:
-                system.push_file(File(name, size))
+    lines = file.readlines()
+    for line in lines:
+        for item in line.strip():
+            number = int(item)
+            grid.append(number + 1)
+
+width = len(lines[0]) - 1
+size = len(grid)
+height = size // width
+visible = height + height + width + width - 4
+
+left = -1
+right = + 1
+up = +width
+down = -width
 
 
+def forest(tree):
+    pass
 
+for index in range(size):
+
+    row = index // width
+    col = index % width
+    print(grid[index], row, col)
+
+
+def show_forest():
+    visible = 0
+    string = io.StringIO()
+    for index_y in range(height):
+        for index_x in range(width):
+            index = index_y * width + index_x
+            if grid[index] < 5:
+                string.write("_")
+                visible += 1
+            else:
+                string.write("*")
+        string.write("\n")
+    print(string.getvalue())
+    print(visible)
+
+show_forest()
