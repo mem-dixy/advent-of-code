62d1773a3459e6d284dc7aeecae4e9d56070a5ca
mem_dixy <mem_dixy@pm.me>
2022-12-09 07:01:37 +0000

    Drawing ready.

--- 09/main.py
+++ 09/main.py
@@ -1,11 +1,5 @@
 import io
 
-VISIBLE = "."
-HIDDEN = "_"
-
-TREE_HOUSE = "$"
-PRETTY_TREES = "%"
-
 grid = []
 forest = []
 scores = []
@@ -13,63 +7,123 @@ scores = []
 # 1 min 10 max
 
 import enum
+import dataclasses
+import types
+import typing
+
+
+EMPTY = "."
+START = "S"
+VISITED = "#"
+
+HEAD = "H"
+TAIL = "T"
 
 DOWN = "D"
 LEFT = "L"
 RIGHT = "R"
 UP = "U"
 
-class Direction(enum.Enum):
-    down = "D"
-    left = "L"
-    right = "R"
-    up = "U"
-
-directions = {
-    DOWN: 0,
-    LEFT: 0,
-    RIGHT: 0,
-    UP: 0,
-}
+@dataclasses.dataclass
+class Direction():
+    direction: str
+    steps: int
 
 
-def find_maze_size():
-    pass
 
-# maze_size
-with open("input.txt") as file:
-    lines = file.readlines()
+
+
+def parse_input(
+) -> list[Direction]:
+    lines = []
+    with open("input.txt") as file:
+        readlines = file.readlines()
+        for line in readlines:
+            row = line.strip().split()
+            if row:
+                direction = Direction(row[0], int(row[1]))
+                lines.append(direction)
+    return lines
+
+def find_maze_limit(
+    lines: list[Direction]
+) -> typing.Dict[str, int]:
+    directions = {
+        DOWN: 0,
+        LEFT: 0,
+        RIGHT: 0,
+        UP: 0,
+    }
     for line in lines:
-        for item in line.strip():
-            number = int(item)
-            grid.append(number)
-            forest.append(HIDDEN)
-            scores.append(0)
+        directions[line.direction] += line.steps
+    width = directions[LEFT] + directions[RIGHT]
+    height = directions[DOWN] + directions[UP]
+    return directions
 
-width = len(lines[0]) - 1
-size = len(grid)
-height = size // width
-
-left = -1
-right = + 1
-up = -width
-down = + width
+def find_maze_size(
+    directions: typing.Dict[str, int]
+) -> typing.Tuple[int]:
+    width = 1 + directions[LEFT] + directions[RIGHT]
+    height = 1 + directions[DOWN] + directions[UP]
+    return (width, height, directions[LEFT], directions[UP])
 
 
-def this_is_edge(index):
-    row = index // width
-    col = index % width
+# generate maze
+lines = parse_input()
+directions = find_maze_limit(lines)
+(width, height, position_x, position_y) = find_maze_size(directions)
+maze = []
+trail = []
+for index_y in range(height):
+    for index_x in range(width):
+        index = index_y * width + index_x
+        maze.append(EMPTY)
+        trail.append(False)
 
-    north = row - 1 < 0
-    south = row + 1 >= height
-    east = col + 1 >= width
-    west = col - 1 < 0
-    return north or south or east or west
+index = position_y * width + position_x
+maze[index] = START
+trail[index] = True
 
 
 
 
+def print_visited():
+    global trail
 
+    visited = 0
+    for cell in trail:
+        if cell:
+            visited += 1
+    print(visited)
+
+def print_maze(draw_maze, draw_trail):
+    global maze
+    global trail
+
+    string = io.StringIO()
+    for index_y in range(height):
+        for index_x in range(width):
+            index = index_y * width + index_x
+            draw_the_maze = str(maze[index])
+            draw_the_trail = VISITED if trail[index] else EMPTY
+
+            if draw_maze and draw_trail:
+                if maze[index] != EMPTY:
+                    draw = draw_the_maze
+                else:
+                    draw = draw_the_trail
+            elif draw_maze:
+                draw = draw_the_maze
+            elif draw_trail:
+                draw = draw_the_trail
+            else:
+                draw = ""
+            string.write(draw)
+        string.write("\n")
+    print(string.getvalue())
+
+print_maze(True, True)
+print_visited()
 # all touching
 # ***
 # ***
