47b14f2f64141302e3566cd3e62e3aaa85745162
mem_dixy <mem_dixy@pm.me>
2022-12-11 05:41:09 +0000

    Parsed monkey input.

--- 11/main.py
+++ 11/main.py
@@ -1,19 +1,66 @@
 import io
 
-def parse_input():
-    lines = []
+def input_read():
+    string = io.StringIO()
     with open("input.txt") as file:
-        readlines = file.readlines()
-        for line in readlines:
-            row = line.strip().split()
-            if row:
-                lines.append(row)
-    return lines
+        lines = file.readlines()
+        for line in lines:
+            string.write(line)
+    return string.getvalue().split("\n\n")
+
+def input_parse(lines):
+    monkeys = []
+    for line in lines:
+        monkeys.append(
+            Monkey(
+                [item.strip() for item in line.split("\n") if item]
+            )
+        )
+    return monkeys
 
 
+def input():
+    string = input_read()
+    return input_parse(string)
 
-lines = parse_input()
-for line in lines:
-    match line:
-        case _:
-            pass
+class Monkey():
+    monkey: int
+    starting_items: list[int]
+    operation_type_addition: bool
+    operation_value: bool
+    test_divisible_by: int
+    if_true_throw_to_monkey: int
+    if_false_throw_to_monkey: int
+
+    def __init__(self, line):
+        self.monkey = int(line[0].split()[-1].split(":")[0])
+
+        items = line[1].split()[2:]
+        self.starting_items = [int(item.split(",")[0]) for item in items]
+        self.starting_items.sort()
+
+        self.operation_type_addition = line[2].split()[-2] == "+"
+
+        value = line[2].split()[-1]
+        self.operation_value = None if value == "old" else int(value)
+
+        self.test_divisible_by = int(line[3].split()[-1])
+        self.if_true_throw_to_monkey = int(line[4].split()[-1])
+        self.if_false_throw_to_monkey = int(line[5].split()[-1])
+
+        self.value = 0
+
+    def operation(self):
+        if self.operation_value:
+            value = self.operation_value
+        else:
+            value = self.value
+
+        if self.operation_type_addition:
+            self.value = self.value + value
+        else:
+            self.value = self.value * value
+
+
+monkeys = input()
+
