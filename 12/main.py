ba77f0d4de7b154899a43923e53e64b52f799a03
mem_dixy <mem_dixy@pm.me>
2022-12-12 07:28:21 +0000

    Maze setup in a class now.

--- 12/main.py
+++ 12/main.py
@@ -4,7 +4,8 @@ import typing
 import types
 
 
-class Cell():
+
+class Agent():
     def __init__(self, index, width):
         self.width = width
         self.index_x = index % width
@@ -13,8 +14,6 @@ class Cell():
     def index(self):
         return self.index_y * self.width + self.index_x
 
-
-class Agent(Cell):
     def down(self):
         self.index_y += 1
 
@@ -27,19 +26,19 @@ class Agent(Cell):
     def up(self):
         self.index_y -= 1
 
+class Dimension():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.depth = width * height
+        ##
+
 
 class Direction(enum.Enum):
     east = enum.auto()
     north = enum.auto()
     south = enum.auto()
     west = enum.auto()
-
-
-class Dimension():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.depth = width * height
 
 
 class Axis():
@@ -91,126 +90,45 @@ class Grid():
         self.axis_y = axis_y
 
 class Cell(Agent):
+    @classmethod
+    def from_index(cls, grid, index):
+        width = grid.axis_x.size
+        height = grid.axis_y.size
+        index_x = index % width
+        index_y = index // width
+        return cls(
+            Grid(
+                Axis(index_x, width),
+                Axis(index_y, height),
+            )
+        )
+
+    @classmethod
+    def clone(cls, grid):
+        return cls(
+            Axis.clone(grid.axis_x),
+            Axis.clone(grid.axis_y),
+        )
+
     def __init__(self, grid):
-
         self.grid = grid
-
-    def move(self, direction):
-
-        match direction:
-            case Direction.east:
-
-                self.index_x += 1
-            case Direction.north:
-                self.index_y -= 1
-            case Direction.east:
-                self.index_y += 1
-            case Direction.west:
-                self.index_x -= 1
 
     def move(self, direction):
         self.grid.move(direction)
 
-    def neighbor(
-        self,
-        test: typing.Callable
-    ) -> list[Direction]:
+    def step(self, direction):
+        grid = Grid.clone(self.grid)
+        grid.move(direction)
+        return grid if grid.valid() else None
 
-        item = Grid.clone(self.grid)
-        path = []
-        if
-        Coordinate
+    def neighbor(self, test):
+        grid = []
+        grid.append(self.step(Direction.east))
+        grid.append(self.step(Direction.north))
+        grid.append(self.step(Direction.south))
+        grid.append(self.step(Direction.west))
+        return filter(None, grid)
 
-        match direction:
-            case Direction.east:
-                self.index_x += 1
-            case Direction.north:
-                self.index_y -= 1
-            case Direction.east:
-                self.index_y += 1
-            case Direction.west:
-                self.index_x -= 1
-
-class Cell(Agent):
-    def __init__(self, grid):
-
-        self.grid = grid
-
-    def move(
-        self,
-        direction: Direction
-    ) -> None:
-
-        match direction:
-            case Direction.east:
-                self.index_x += 1
-            case Direction.north:
-                self.index_y -= 1
-            case Direction.east:
-                self.index_y += 1
-            case Direction.west:
-                self.index_x -= 1
-
-    def move(
-        self,
-        direction: Direction
-    ) -> None:
-
-        match direction:
-            case Direction.east:
-                self.index_x += 1
-            case Direction.north:
-                self.index_y -= 1
-            case Direction.east:
-                self.index_y += 1
-            case Direction.west:
-                self.index_x -= 1
-
-    def neighbor(
-        self,
-        test: typing.Callable
-    ) -> list[Direction]:
-
-        item = Grid.clone(self.grid)
-        path = []
-        if
-        Coordinate
-
-        match direction:
-            case Direction.east:
-                self.index_x += 1
-            case Direction.north:
-                self.index_y -= 1
-            case Direction.east:
-                self.index_y += 1
-            case Direction.west:
-                self.index_x -= 1
-
-
-CLIMB_LIMIT = 1
-
-
-data = None
-with open("input.txt") as file:
-    data = file.read()
-
-
-def make_maze(data):
-    data_split = data.split()
-    width = len(data_split[0])
-    height = len(data_split)
-    axis_x = Axis(0, width)
-    axis_y = Axis(0, height)
-    maze = Grid(axis_x, axis_y)
-    return maze
-
-
-maze = make_maze(data)
-
-data_lookup = [item for item in data if item.isalpha()]
-
-player = Agent(data_lookup.index("S"), dimension.width)
-goal = Cell(data_lookup.index("E"), dimension.width)
 
 
 def elevation(letter):
@@ -220,15 +138,58 @@ def elevation(letter):
         letter = "z"
     return ord(letter) - ord("a")
 
+class Maze(Agent):
+
+    def __init__(self, data):
+
+        data_split = data.split()
+        width = len(data_split[0])
+        height = len(data_split)
+        axis_x = Axis(0, width)
+        axis_y = Axis(0, height)
+        grid = Grid(axis_x, axis_y)
+
+        self.grid = grid
+        self.heightmap = []
+        self.draw = []
+        self.extra = []
+        self.distance = []
+
+        data_lookup = [item for item in data if item.isalpha()]
+
+        self.start = Agent(data_lookup.index("S"), width)
+        self.finish = Agent(data_lookup.index("E"), width)
+
+        heightmap = [elevation(item) for item in data_lookup]
+        size = len(heightmap)
+        distance = [None] * size
+        distance[self.start.index()] = 0
+
+
+
+
+
+CLIMB_LIMIT = 1
+
+maze = None
+with open("input.txt") as file:
+    data = file.read()
+    maze = Maze(data)
+
+
+
+
+
+
+
+
 
 def climbable(here, goto):
     return here + CLIMB_LIMIT <= goto
 
 
-heightmap = [elevation(item) for item in data_lookup]
-distance = [None] * size
-distance[player.index()] = 0
 
-
+size = 1
 for round in range(size):
+    pass
 
