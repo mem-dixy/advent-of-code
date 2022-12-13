b16d0644c475007a734f308df5c3c2ad954770f8
mem_dixy <mem_dixy@pm.me>
2022-12-13 05:53:44 +0000

    Almost working.

--- 13/main.py
+++ 13/main.py
@@ -13,84 +13,49 @@ class Type(enum.Enum):
     ARRAY = value_type([0])
 
 
-test = value_type(4)
-
-
-a = [1, 2, 3]
-b = [4, 5]
-
-c = zip(a, b, strict=False)
-d = list(c)
-print(d)
-for item in d:
-    print(item)
-
-print(Type.NUMBER.value)
-print(Type.ARRAY.value)
 index = 1
 result = []
 
 def compare(left, right):
-    while True:
-        match (value_type(left), value_type(right)):
-            case (Type.NUMBER.value, Type.NUMBER.value):
-                if left < right:
-                    return True
-                if left > right:
-                    return False
-            case (Type.ARRAY.value, Type.ARRAY.value):
-                for zipper in zip(left, right, strict=False):
-                    match compare(*zipper):
-                        case True:
-                            return True
-                        case False:
-                            return False
-                if len(left) < len(right):
-                    return True
-                if len(left) > len(right):
-                    return False
-            case (Type.NUMBER.value, Type.ARRAY.value):
-                match compare(left, [right]):
+    match (value_type(left), value_type(right)):
+        case (Type.NUMBER.value, Type.NUMBER.value):
+            if left < right:
+                return True
+            if left > right:
+                return False
+        case (Type.ARRAY.value, Type.ARRAY.value):
+            for zipper in zip(left, right, strict=False):
+                match compare(*zipper):
                     case True:
                         return True
                     case False:
                         return False
-            case (Type.ARRAY.value, Type.NUMBER.value):
-                match compare([left], right):
-                    case True:
-                        return True
-                    case False:
-                        return False
-            case _:
-                test = False
-                print("FAIL")
-        return "BOO"
-
-
-array = array[0:1]
-for item in array:
-    (one, two) = item
-    print(one, two)
-    (one, two) = (value_type(one), value_type(two))
-
-    test = None
-    match (one, two):
-        case (Type.NUMBER.value as left, Type.NUMBER.value as right):
-            test = True if left < right else test
-            test = False if left > right else test
-        case (Type.ARRAY.value as left, Type.ARRAY.value as right):
-            test = True if left < right else test
-            test = False if left > right else test
-        case Type.ARRAY.value:
-            print("ARRAY")
+            if len(left) < len(right):
+                return True
+            if len(left) > len(right):
+                return False
+        case (Type.NUMBER.value, Type.ARRAY.value):
+            match compare([left], right):
+                case True:
+                    return True
+                case False:
+                    return False
+        case (Type.ARRAY.value, Type.NUMBER.value):
+            match compare(left, [right]):
+                case True:
+                    return True
+                case False:
+                    return False
         case _:
-            test = False
             print("FAIL")
+            return False
+    return True
 
-    if True:
+for item in array:
+    if compare(*item):
         result.append(index)
     index += 1
 
-
+print(result)
 final = sum(result)
 print(final)
